5b16761de143d7b61fc1a78995d948efd72f3a4d
SedickIyamah <43197432+SedickIyamah@users.noreply.github.com>
2019-04-08 12:31:12 -0700

    Update message.cpp

--- message.cpp
+++ message.cpp
@@ -1,21 +1,67 @@
 #include "message.h"
+#include <ctime>
+#include <chrono>
 
-string Message::get_message(){
-    return message;
+
+Message::Message(){
+    string text = "XXX";
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer[80];
+
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer,sizeof(buffer),"%m-%d-%Y %H:%M:%S",timeinfo);
+    std::string str(buffer);
+    string timeStamp = str;
 }
 
-void Message::set_message(string m){
-    message = m;
+string message::GetObfuscation()
+{
+//    obfuscationCode = n;
+    return obfuscationCode;
 }
 
-int Message::get_time(){
-    return _time;
+/*bool message::ValidateObfuscation(string n)
+{
+    bool is_valid = false;
+    if(obfuscationCode == n)
+    {
+        is_valid = true;
+    }
+    return is_valid;
+}*/
+
+void message::setText(string message)
+{
+    text = message;
+    return;
+}
+void message::PrintInfo()
+{
+    cout << timeStamp << endl;
+    cout << text << endl;
+    return;
 }
 
-void Message::set_time(int t){
-    _time = t;
+void message::SetTimeStamp()
+{
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer[80];
+
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer,sizeof(buffer),"%m-%d-%Y %H:%M:%S",timeinfo);
+    std::string str(buffer);
+    timeStamp = str;
+    return;
 }
 
-string Message::to_string(){
-    return "Time: " + std::to_string(_time) + ", Message: " + message + "\n";
-}  
+string message::ToString()
+{
+    string info = text + " | " + timeStamp;
+    return info;
+}
